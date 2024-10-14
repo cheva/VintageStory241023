17da3820570bdb26aaf8091c4e20889ce5a8e58a
Dee <dee.covan@gmail.com>
2024-10-14 23:53:10 +0300

    Have 100 Copper and 100 Clay

--- run1280.bat
+++ run1280.bat
@@ -1,3 +1,4 @@
 C:\Programs\QRes.exe /x:1280 /y:720
 copy clientsettings.json.fix.1280x720 clientsettings.json
 "C:\Games\VintageStory 1.20\PlayVintageStory.exe"
+copy clientsettings.json clientsettings.json.fix.1280x720
